1c5b2d60b885d966885bb077d4bb0afee36413c9
Aaron <107720212+aaronab3r@users.noreply.github.com>
2025-03-18 17:20:29 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoApp from "./TodoApp";
+
+const App: React.FC = () => {
+    return(
+        <div>
+            <TodoApp />
+        </div>
+    );
+};
+
+
+export default App;
